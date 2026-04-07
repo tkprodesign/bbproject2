85075cbffa4aef66db967f1ab3dfb9e0854cd0f3
Tk Designs <103645938+tkprodesign@users.noreply.github.com>
2026-04-08 00:44:27 +0100

    Allow admin dashboard access and improve mobile control-panel fit (#51)

--- control-panel/index.php
+++ control-panel/index.php
@@ -7,6 +7,7 @@
     <title>Control Panel</title>
     <link rel="stylesheet" href="/assets/stylesheets/control-panel.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="/assets/stylesheets/tab/control-panel.css?v=<?php echo time(); ?>" media="screen and (max-width: 1000px)">
+    <link rel="stylesheet" href="/assets/stylesheets/mobile/control-panel.css?v=<?php echo time(); ?>" media="screen and (max-width: 720px)">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
