d0211ff0f8ff90d84dac95dd7bd8ac8cfa90bae5
Tk Designs <103645938+tkprodesign@users.noreply.github.com>
2026-04-07 03:03:14 +0100

    Restructure header content and polish primary navigation styling

--- htdocs/index.php
+++ htdocs/index.php
@@ -27,8 +27,9 @@
 
     <!-- Favicon -->
     
-    <link rel="stylesheet" href="/assets/stylesheets/home2.css">
-    <link rel="stylesheet" media="screen and (max-width: 1000px)" href="/assets/stylesheets/tab/home2.css">
+    <link rel="stylesheet" href="/assets/stylesheets/main.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="/assets/stylesheets/home.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" media="screen and (max-width: 1000px)" href="/assets/stylesheets/tab/home.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 </head>
